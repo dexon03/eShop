2697a09b65db0d1332279260628027c932b37d93
dexon03 <dima0958757803@gmail.com>
2023-03-22 19:46:54 +0200

    dependencies db context

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Application.Data;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+        services.AddDbContext<DbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+        return services;
+    }
+}
