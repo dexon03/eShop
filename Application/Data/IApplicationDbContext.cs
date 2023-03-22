0f528ebeb276d949af65492bd88b7c21154287a6
dexon03 <dima0958757803@gmail.com>
2023-03-22 19:46:18 +0200

    added db context

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Application.Data;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    
+    public DbSet<Category> Categories { get; set; }
+}
