6f4c94be2d8741d56bcd89d26871b2ec7238a6de
dexon03 <dima0958757803@gmail.com>
2023-03-22 19:45:03 +0200

    added models

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int AvailableCount { get; set; }
+    public string Description { get; set; }
+    public string ShortDescription { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid VendorId { get; set; }
+    public string ImageUrl { get; set; }
+
+    // Reference navigation properties
+    public Category Category { get; set; }
+    public Vendor Vendor { get; set; }
+}
---
+++ Domain/Entities/Vendor.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities;
+
+public class Vendor
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
