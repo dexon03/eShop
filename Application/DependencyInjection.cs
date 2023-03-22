1a4a0a5626f7c54337a13549a40d1fce3bc8ad7b
dexon03 <dima0958757803@gmail.com>
2023-03-22 13:25:50 +0200

    init

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(assembly);
+        });
+        services.AddValidatorsFromAssembly(assembly);
+        services.AddAutoMapper(assembly);
+        return services;
+    }
+}
