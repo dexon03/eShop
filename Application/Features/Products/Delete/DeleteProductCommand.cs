02555a78e900922ca6ad1cc084312595c964b8ea
dexon03 <dima0958757803@gmail.com>
2023-03-31 17:10:59 +0300

    added delete use case to product mediatr

---
+++ Application/Features/Products/Delete/DeleteProductCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Application.Features.Products.Delete;
+
+public record DeleteProductCommand(Guid Id) : IRequest;
---
+++ Application/Features/Products/Delete/DeleteProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Data;
+using MediatR;
+
+namespace Application.Features.Products.Delete;
+
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteProductCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _context.Products.FindAsync(request.Id);
+        
+        if (product is null)
+        {
+            throw new KeyNotFoundException("Product not found");
+        }
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
