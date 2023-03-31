6a830512be6eae9381a02bbb42c6ac38ad6541d5
dexon03 <dima0958757803@gmail.com>
2023-03-31 17:10:42 +0300

    added server-side validation for product

---
+++ Application/Features/Products/Create/CreateProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Features.Products.Create;
+
+public class CreateProductValidator : AbstractValidator<Product>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MinimumLength(2);
+        RuleFor(x => x.Price).NotEmpty().GreaterThan(0);
+        RuleFor(x => x.AvailableCount).NotEmpty().GreaterThan(-1);
+        RuleFor(x => x.Description).NotEmpty().MinimumLength(10);
+        RuleFor(x => x.ShortDescription).NotEmpty().MinimumLength(5);
+        RuleFor(x => x.CategoryId).NotEmpty();
+        RuleFor(x => x.VendorId).NotEmpty();
+        RuleFor(x => x.ImageUrl).NotEmpty();
+    }
+}
